b10a76d922e5ccf4cf7f10addd65de31f281bd22
FoliFF <olof@hisham.se>
2022-05-30 22:43:10 +0200

    Updated task 3.2
    Solved some issues with task 3.2

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+
+    render() {
+        return (
+            <div className="main-view">
+                <div>Inception</div>
+                <div>Lord of the Rings</div>
+                <div>Your Name</div>
+            </div>
+        );
+    }
+}
