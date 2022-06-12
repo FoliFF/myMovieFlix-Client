14b0ef2c8d556a3a3a5548a387fe4936b92b8527
FoliFF <olof@hisham.se>
2022-06-12 18:55:18 +0200

    Task 3.5

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 import { LoginView } from '../../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export class MainView extends React.Component {
 
@@ -94,15 +96,21 @@ export class MainView extends React.Component {
             return <div className='main-view' />;
 
         return (
-            <div className="main-view">
+            <Row className="main-view justify-content-md-center">
                 {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
                 {selectedMovie
-                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                    ? (
+                        <Col md={8}>
+                            <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                        </Col>
+                    )
                     : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} /> //onMovieClick={(movie) => { this.setSelectedMovie(movie)
+                        <Col md={3}>
+                            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
+                        </Col>
                     ))
                 }
-            </div>
+            </Row>
         );
     }
 }
--- src/login-view/login-view.jsx
+++ src/login-view/login-view.jsx
@@ -1,55 +1,68 @@
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export function LoginView(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(username, password);
-    };
-    /* 
-        Send a request to the server for authentication
-        then call props.onLoggedIn(username) 
-        props.onLoggedIn(username); 
-    */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    //Send a request to the server for authentication then call props.onLoggedIn(username) 
+    props.onLoggedIn(username);
+  };
 
-    /* const handleSubmit = (e) => {
-        e.preventDefault();
-        const isReq = validate();
-        if (isReq) {
-          axios
-            .post('https://movie-api-21197.herokuapp.com/login', {
-              Username: username,
-              Password: password,
-            })
-            .then((response) => {
-              const data = response.data;
-              props.onLoggedIn(data);
-            })
-            .catch((e) => {
-              console.log('no such user');
-              alert(
-                'Wrong Username or Password. If you are new here, please register first.'
-              );
-            });
-        }
-      }; */
+  /* THIS IS FROM ARON SUNDAY AND ME TRYING TO UNDERSTAND THE CODE 
 
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      const isReq = validate();
+      if (isReq) {
+        axios
+          .post('https://movie-api-21197.herokuapp.com/login', {
+            Username: username,
+            Password: password,
+          })
+          .then((response) => {
+            const data = response.data;
+            props.onLoggedIn(data);
+          })
+          .catch((e) => {
+            console.log('no such user');
+            alert(
+              'Wrong Username or Password. If you are new here, please register first.'
+            );
+          });
+      }
+    }; */
 
+  return (
+    <Form>
+      <Form.Group controlId='fromUsername'>
+        <Form.Label>Username</Form.Label>
+        <Form.Control type='text' onChange={e => setUsername(e.target.value)} />
+      </Form.Group>
 
+      <Form.Group controlId='fromPassword'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control type='Password' onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
 
-    return (
-        <form>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <button type="sumbit" onClick={handleSubmit}>Submit</button>
-        </form>
-    );
+      <Button variant='primary' type='submit' onClick={handleSubmit}>Submit</Button>
+    </Form>
+
+    /* Code for task 3.4
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="sumbit" onClick={handleSubmit}>Submit</button>
+    </form> */
+  );
 }
